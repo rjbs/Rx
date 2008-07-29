c68ed9f1998cae191168a4e8fa3a37e8043bed26
Ricardo SIGNES <rjbs@cpan.org>
2008-07-28 21:24:15 -0400

    optional map entries

--- lib/Data/Rx/CoreType/map.pm
+++ lib/Data/Rx/CoreType/map.pm
@@ -14,10 +14,17 @@ sub new {
     unless $arg->{contents} and (ref $arg->{contents} eq 'HASH');
   Carp::croak("unknown arguments to new") unless keys %$arg == 1;
 
-  my %content_check = (
-    map {; $_ => Data::Rx->new->make_checker($arg->{contents}{$_}) }
-        keys %{ $arg->{contents} }
-  );
+  my %content_check;
+  
+  for my $key (keys %{ $arg->{contents} }) {
+    my %key_arg  = %{ $arg->{contents}{$key} };
+    my $optional = delete $key_arg{optional};
+
+    $content_check{ $key } = {
+      optional => $optional, 
+      checker  => Data::Rx->new->make_checker(\%key_arg),
+    };
+  };
 
   return bless { content_check => \%content_check } => $class;
 }
@@ -28,11 +35,12 @@ sub check {
   return unless
     ! Scalar::Util::blessed($value) and ref $value eq 'HASH';
   
-  my %c_check = %{ $self->{content_check} };
-  return unless keys %$value == keys %c_check;
+  my $c_check = $self->{content_check};
+  return unless Data::Rx::Util->_x_subset_keys_y($value, $c_check);
 
-  for my $key (keys %c_check) {
-    return unless exists $value->{$key} and $c_check{$key}->($value->{$key});
+  for my $key (keys %$c_check) {
+    return if not $c_check->{$key}{optional} and not exists $value->{$key};
+    return unless $c_check->{$key}{checker}->($value->{$key});
   }
 
   return 1;
