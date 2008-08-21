aac9cff00903a1c58216b686337de1f0cddc446e
Ricardo SIGNES <rjbs@cpan.org>
2008-08-21 10:04:49 -0400

    puttering at test runner

---
+++ ruby/Rx.rb
@@ -0,0 +1,10 @@
+
+class Rx
+  def make_schema(schema)
+  end
+
+  class Error
+  end
+end
+
+
--- ruby/rx-test.rb
+++ ruby/rx-test.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/ruby
+require 'find'
+require 'pathname'
 require 'rubygems'
 require 'json'
+
+require './ruby/Rx.rb'
 
 test_data   = { }
 test_schema = { }
@@ -13,6 +17,67 @@ Dir.open('spec/data').each { |file|
   file.sub!(/\.json$/, '')
 
   test_data[file] = JSON.parse(json)
+
+  if test_data[file].instance_of?(Array) then
+    new_data = { }
+    test_data[file].each { |e| new_data[e] = e }
+    test_data[file] = new_data
+  end
+
+  test_data[file].each_pair { |k,v|
+    boxed = JSON.parse("[ #{ v } ]")
+    test_data[file][k] = boxed[0]
+  }
 }
 
-puts test_data
+class TAP_Emitter
+  def ok(bool, desc)
+    @i = 0 if @i === nil
+    @i += 1
+    printf("%s %s %s\n", bool ? 'ok' : 'not ok', @i, desc);
+  end
+end
+
+Find.find('spec/schemata') { |path|
+  next unless File.file?(path)
+
+  leaf = Pathname.new(path).
+         relative_path_from( Pathname.new('spec/schemata') ).
+         to_s
+
+  leaf.sub!(/\.json$/, '')
+
+  json = File.open(path).read
+  test_schema[leaf] = JSON.parse(json)
+}
+
+rx = Rx.new
+tap = TAP_Emitter.new
+
+test_schema.keys.sort.each { |schema_name|
+  schema_test_desc = test_schema[ schema_name ]
+
+  begin
+    schema = rx.make_schema(schema_test_desc['schema'])
+  rescue Rx::Error => e
+    if schema_test_desc['invalid'] then
+      tap.ok(true, "BAD SCHEMA: #{ schema_name }")
+      next
+    end
+
+    throw e
+  end
+
+  if not schema then
+    tap.ok(false, "no schema for valid input (#{schema_name})")
+    next
+  end
+
+  if schema_test_desc['invalid'] then
+    tap.ok(false, "BAD SCHEMA: #{ schema_name }")
+    next
+  end
+}
+
+# puts test_data.inspect
+# puts test_schema.inspect
