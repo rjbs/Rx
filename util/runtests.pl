fb698ca7cb0dc1f4a45b0a9a8eb4c5f317a196f2
Ricardo SIGNES <rjbs@cpan.org>
2008-08-22 19:08:31 -0400

    reorg by lang

--- util/runtests.pl
+++ util/runtests.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 use TAP::Harness;
@@ -5,7 +6,7 @@ use TAP::Harness;
 my $harness = TAP::Harness->new({
   exec => sub {
     my ($self, $filename) = @_;
-    return [ $^X, -I => 'perl', $filename ] if $filename =~ /\.t$/;
+    return [ $^X, -I => 'perl/lib', -I => 'perl/t/lib', $filename ] if $filename =~ /\.t$/;
     return [ 'java', -jar => 'js.jar', $filename ] if $filename =~ /\.js$/;
     return [ 'ruby', -I => 'ruby', $filename ] if $filename =~ /\.rb$/;
     return [ 'python', $filename ] if $filename =~ /\.py$/;
@@ -15,8 +16,8 @@ my $harness = TAP::Harness->new({
 # You may only choose one of 'exec', 'stream', 'tap' or 'source' at - line 12
 
 $harness->runtests(qw(
-  t/spec.t
-  t/util-range.t
+  perl/t/spec.t
+  perl/t/util-range.t
   ruby/rx-test.rb
   python/rx-test.py
   js/rx/test/runner.js
