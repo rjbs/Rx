2e93a08e77d310ffbe75d097ba5dfee484248f45
Ricardo SIGNES <rjbs@cpan.org>
2008-08-24 08:48:16 -0400

    any of for perl

--- perl/lib/Data/Rx/CoreType/any.pm
+++ perl/lib/Data/Rx/CoreType/any.pm
@@ -3,7 +3,34 @@ use warnings;
 package Data::Rx::CoreType::any;
 use base 'Data::Rx::CoreType';
 
-sub check { return 1 }
+use Scalar::Util ();
+
+sub new {
+  my ($class, $arg, $rx) = @_;
+
+  Carp::croak("unknown arguments to new")
+    unless Data::Rx::Util->_x_subset_keys_y($arg, { of  => 1});
+
+  my $self = bless { } => $class;
+
+  if (my $of = $arg->{of}) {
+    Carp::croak("invalid 'of' argument to //any") unless
+      Scalar::Util::reftype $of eq 'ARRAY';
+    
+    $self->{of} = [ map {; $rx->make_schema($_) } @$of ];
+  }
+
+  return $self;
+}
+
+sub check {
+  return 1 unless $_[0]->{of};
+
+  my ($self, $value) = @_;
+  
+  $_->check($value) && return 1 for @{ $self->{of} }
+  return;
+}
 
 sub authority { '' }
 sub subname   { 'any' }
