1f8e80a874bfe4b9bdaa064f1d2fc413d8640158
Ricardo SIGNES <rjbs@cpan.org>
2009-04-14 23:06:05 -0400

    gosh, i forgot the new test rig

---
+++ perl/t/lib/Test/RxTester.pm
@@ -0,0 +1,187 @@
+use strict;
+use warnings;
+package Test::RxTester;
+
+use autodie;
+use Data::Rx;
+use File::Find::Rule;
+use JSON::XS ();
+use Test::More;
+
+sub new {
+  my ($class, $arg) = @_;
+  $arg ||= {};
+  
+  my $guts = {
+    schema_files => $arg->{schema_files},
+    data_files   => $arg->{data_files},
+  };
+
+  my $self = bless $guts => $class;
+
+  $self->_load_data_files;
+  $self->_load_schema_files;
+
+  return $self;
+}
+
+sub _load_data_files {
+  my ($self) = @_;
+
+  for my $fn (@{ $self->{data_files} }) {
+    my ($name) = File::Basename::fileparse($fn);
+    $name =~ s{\.json}{};
+
+    die "already loaded data called $name" if exists $self->{data}{$name};
+
+    my $data = $self->slurp_json($fn);
+    $data = { map { $_ => $_ } @$data } if ref $data eq 'ARRAY';
+
+    $self->{data}{$name} = $data;
+  }
+}
+
+sub _load_schema_files {
+  my ($self) = @_;
+
+  for my $fn (@{ $self->{schema_files} }) {
+    my ($name) = File::Basename::fileparse($fn);
+    $name =~ s{\.json}{};
+
+    die "already loaded schema spec tests for $name"
+      if exists $self->{spec}{$name};
+
+    my $data = $self->slurp_json($fn);
+    $data = { map { $_ => $_ } @$data } if ref $data eq 'ARRAY';
+
+    $self->{spec}{$name} = {
+      invalid => $data->{invalid},
+      schema  => $data->{schema},
+      expect  => { },
+    };
+
+    for my $pf (qw(pass fail)) {
+      for my $source (keys %{ $data->{$pf} }) {
+        my $spec = $data->{$pf}{ $source };
+        my $ref  = ref $spec;
+
+        my %entries
+          = $ref eq 'HASH'  ? %$spec
+          : $ref eq 'ARRAY' ? (map {; $_ => undef } @$spec)
+          : $ref            ? die("invalid test spec: $spec")
+          : $spec eq '*'    ? (map {; $_ => undef } keys %{$self->test_data($source)})
+          : Carp::croak("invalid test spec: $spec");
+
+        $self->{spec}{$name}{expect}{$source}{$pf} = \%entries;
+      }
+    }
+  }
+}
+
+sub test_data {
+  my ($self, $name) = @_;
+
+  die "no such test data: $name" unless exists $self->{data}{$name};
+  return $self->{data}{$name};
+}
+
+sub _decode_json {
+  my ($self, $json_str) = @_;
+  $self->{__json} ||= JSON::XS->new;
+  $self->{__json}->decode($json_str);
+}
+
+sub slurp_json {
+  my ($self, $fn) = @_;
+
+  my $json = do { local $/; open my $fh, '<', $fn; <$fh> };
+  my $data = eval { $self->_decode_json($json) };
+  die "$@ (in $fn)" unless $data;
+  return $data;
+}
+
+my $fudge = {
+  int => { str => "Perl has trouble with num/str distinction", },
+  num => { str => "Perl has trouble with num/str distinction", },
+  str => { num => "Perl has trouble with num/str distinction", },
+
+  'num-0'           => { str => "Perl has trouble with num/str distinction", },
+  'int-0'           => { str => "Perl has trouble with num/str distinction", },
+  'int-range-empty' => { str => "Perl has trouble with num/str distinction", },
+  'num-range' => {
+    str => {
+      '5.1' => 'Perl has trouble with num/str distinction',
+    },
+  },
+
+  'array-3-int' => {
+    arr => {
+      '0-s1-1' => 'Perl has trouble with num/str distinction',
+    },
+  },
+};
+
+sub fudge_reason {
+  my ($schema, $source, $entry) = @_;
+
+  return unless $fudge->{$schema}
+     and my $se_reason = $fudge->{$schema}{$source};
+
+  return $se_reason if ! ref $se_reason;
+  return unless my $reason = $se_reason->{$entry};
+  return $reason;
+}
+
+sub assert_pass {
+  my ($self, $schema, $input, $schema_name, $input_name) = @_;
+  ok($schema->check($input),   "VALID  : $input_name against $schema_name");
+}
+
+sub assert_fail {
+  my ($self, $schema, $input, $schema_name, $input_name) = @_;
+  ok(! $schema->check($input), "INVALID: $input_name against $schema_name");
+}
+
+sub run_tests {
+  my ($self) = @_;
+
+  my $spec_data = $self->{spec};
+  SPEC: for my $spec_name (sort keys %$spec_data) {
+    my $spec = $spec_data->{ $spec_name };
+
+    my $rx     = Data::Rx->new;
+    my $schema = eval { $rx->make_schema($spec->{schema}) };
+    my $error  = $@;
+
+    if ($spec->{invalid}) {
+      ok($error && ! $schema, "BAD SCHEMA: $spec_name");
+      next SPEC;
+    }
+
+    Carp::croak("couldn't produce schema for valid input ($spec_name): $error")
+      unless $schema;
+
+    for my $pf (qw(pass fail)) {
+      my $method = "assert_$pf";
+
+      my @sources = keys %{ $spec->{expect} };
+
+      for my $source (@sources) {
+        my $entries = $spec->{expect}{ $source }{ $pf };
+        for my $entry (keys %$entries) {
+          my $json  = $self->test_data($source)->{ $entry };
+
+          my $input = $self->_decode_json("[ $json ]")->[0];
+
+          TODO: {
+            my $reason = fudge_reason($spec_name, $source, $entry);
+            local $TODO = $reason if $reason;
+            $self->$method($schema, $input, $spec_name, "$source/$entry");
+          }
+        }
+      }
+    }
+  }
+}
+
+1;
