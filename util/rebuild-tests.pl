7838881649ac6153b2d33a71e9fe330ea6afc4a5
Ricardo Signes <rjbs@cpan.org>
2010-06-23 08:08:58 -0400

    improve the spec build script

--- util/rebuild-tests.pl
+++ util/rebuild-tests.pl
@@ -5,10 +5,11 @@ use warnings;
 use autodie;
 use File::Find::Rule;
 use JSON 2 ();
+use Params::Util qw(_HASHLIKE);
 use Scalar::Util;
 use Try::Tiny;
 
-my @test_sets;
+my %test_set;
 
 # LOAD THE DATA FILES
 my %data_set;
@@ -32,7 +33,7 @@ my @schema_files = File::Find::Rule->file->in('spec/schemata');
 
 SCHEMA: for my $file (@schema_files) {
   (my $name = $file) =~ s{\.json}{};
-  $name =~ s{spec/data/}{};
+  $name =~ s{spec/schemata/}{};
 
   my $spec = slurp_json($file);
 
@@ -40,8 +41,7 @@ SCHEMA: for my $file (@schema_files) {
 
   if ($spec->{invalid}) {
     $set->{invalid} = \1;
-    $set->{name} = $name;
-    push @test_sets, $set;
+    $test_set{ $name } = $set;
     next SCHEMA;
   }
 
@@ -49,25 +49,25 @@ SCHEMA: for my $file (@schema_files) {
     for my $source (keys %{ $spec->{$pf} }) {
       my $expect = normalize($spec->{$pf}{ $source }, $data_set{ $source });
 
-      my $tests = $set->{tests} = [ ];
+      my $test = $set->{test} = { };
 
       for my $entry (keys %$expect) {
         die "bogus test input $name $source $pf"
           if $pf eq 'fail' and ! $expect->{$entry};
 
-        push @$tests, {
+        $test->{"$source/$entry"} = {
           input  => $data_set{ $source }{ $entry },
           errors => $expect->{$entry} || [],
         };
       }
 
-      push @test_sets, $set;
+      $test_set{ $name } = $set;
     }
   }
 }
 
 open my $fh, '>', 'spec.json';
-print {$fh} JSON->new->pretty->canonical->encode(\@test_sets);
+print {$fh} JSON->new->pretty->canonical->encode(\%test_set);
 close $fh;
 
 sub normalize {
