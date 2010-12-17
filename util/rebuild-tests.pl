d1a0b5c0ca221991d6aefbb2e48fa41998e188de
Ronald J Kimball <rjk@tamias.net>
2010-12-17 12:34:18 -0500

    Copy tests from rec-key-rest-longuri.json to rec-key-rest.json
    Replaces the comments, so rebuild-tests.pl no longer needs JSON->relaxed.

--- util/rebuild-tests.pl
+++ util/rebuild-tests.pl
@@ -108,7 +108,7 @@ sub normalize {
 my $JSON;
 sub slurp_json {
   my ($fn) = @_;
-  $JSON ||= JSON->new->relaxed;
+  $JSON ||= JSON->new;
 
   my $json = do { local $/; open my $fh, '<', $fn; <$fh> };
   my $data = eval { $JSON->decode($json) };
