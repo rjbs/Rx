32fdd353fbc1afbfb2caf0931ab121efb4f92900
Ronald J Kimball <rjk@tamias.net>
2011-03-07 23:12:14 -0500

    Throw error for schema for composed type with extra args in rx.js

--- js/rx.js
+++ js/rx.js
@@ -61,6 +61,8 @@ Rx.prototype.makeSchema = function (schema) {
   var typeChecker = this.typeFor(schema.type);
 
   if (typeof(typeChecker) == 'object') {
+    if (! Rx.Util._x_subset_keys_y(schema, { type: true }))
+      throw new Rx.Error('composed type does not take check arguments');
     return this.makeSchema(typeChecker.schema);
   } else {
     return new typeChecker(schema, this);
