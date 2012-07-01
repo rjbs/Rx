3cdcb1bf72a3146fca4f1765be559220f9a8aaff
Ricardo Signes <rjbs@cpan.org>
2012-07-01 11:55:25 -0400

    make CommonType::EasyNew

---
+++ perl/lib/Data/Rx/CommonType/EasyNew.pm
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+package Data::Rx::CommonType::EasyNew;
+# ABSTRACT: base class for core Rx types, with some defaults
+use parent 'Data::Rx::CommonType';
+
+sub guts_from_arg {
+  my ($class, $arg, $rx, $type = @_;
+
+  Carp::croak "$class does not take check arguments" if %$arg;
+
+  return {};
+}
+
+sub new_checker {
+  my ($class, $arg, $rx, $type) = @_;
+
+  my $guts = $self->guts_from_arg($arg, $rx, $type);
+
+  # Carp::confess "underscore-led entry in guts!" if grep /\A_/, keys %$guts;
+  $guts->{_type} = $type;
+  $guts->{_rx}   = $rx;
+
+  bless $guts => $class;
+}
+
+sub type { $_[0]->{_type} }
+
+sub rx { $_[0]->{_rx} }
+
+1;
